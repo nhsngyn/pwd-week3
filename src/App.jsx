7c2e0cdc5ac1d7843ace3886a8b8dfc693846e3f
nhsngyn <nhsngyn@gmail.com>
2025-09-20 22:09:34 +0900

    22:09 수정사항

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Routes, Rout} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
